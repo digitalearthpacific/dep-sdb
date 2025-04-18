104f2023e1a4fa6eb6f0067e0ddbb4675703b132
Alex Leith <alex@auspatious.com>
2025-04-18 12:36:30 +1000

    Update print-tasks to be simpler, and not include hawaii

--- src/print_tasks.py
+++ src/print_tasks.py
@@ -1,14 +1,13 @@
 import json
 import sys
 
-import geopandas as gpd
+import requests
 import typer
 from boto3 import client
 from dep_tools.aws import object_exists
 from dep_tools.namers import S3ItemPath
 
-COASTLINE_GRID = "https://dep-public-staging.s3.us-west-2.amazonaws.com/dep_ls_coastlines/raw/buffered_coastline_grid.gpkg"
-
+TILES_LIST = "https://dep-public-staging.s3.us-west-2.amazonaws.com/dep_ls_coastlines/raw/non_hawaii_tiles.txt"
 
 app = typer.Typer()
 
@@ -19,21 +18,20 @@ def print_tasks(
     limit: int = None,
     output_bucket: str = "dep-public-staging",
     output_prefix: str = None,
-    datetime: str = None,
-    version: str = None,
+    datetime: str = "2025",
+    version: str = "0.0.0",
 ):
-    # Load the coastline grid
-    coastlines = gpd.read_file(COASTLINE_GRID)
+    # Load the coastline non-hawaii tiles list
+    tile_ids = requests.get(TILES_LIST).text.splitlines()
 
-    tasks = [tile_id for tile_id in coastlines["tile_id"]]
     if limit is not None:
-        tasks = tasks[:limit]
+        tile_ids = tile_ids[:limit]
 
     if not overwrite:
         s3_client = client("s3")
-        valid_tasks = []
+        valid_tile_ids = []
 
-        for task in tasks:
+        for tile_id in tile_ids:
             itempath = S3ItemPath(
                 bucket=output_bucket,
                 sensor="s2",
@@ -41,17 +39,17 @@ def print_tasks(
                 version=version,
                 time=datetime,
             )
-            stac_path = itempath.stac_path(task["tile-id"].split(","))
+            stac_path = itempath.stac_path(tile_id)
 
             if output_prefix is not None:
                 stac_path = f"{output_prefix}/{stac_path}"
 
             if not object_exists(output_bucket, stac_path, client=s3_client):
-                valid_tasks.append(task)
+                valid_tile_ids.append(tile_id)
 
-        tasks = valid_tasks
+        tile_ids = valid_tile_ids
 
-    json.dump(tasks, sys.stdout)
+    json.dump(tile_ids, sys.stdout)
 
 
 if __name__ == "__main__":
